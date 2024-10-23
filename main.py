e34eca7ddde70d98e859fad8db86b7129f80afbd
noahpaquin <noahpaquin7@gmail.com>
2024-10-23 13:59:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-# Put your function here
+def DecreaseElements():
+    vals = [[96, 5, 23, 16, 45, 63], [20, 106, 50, 27, 38, 15]]
 
 
-# testing
-nums = [[96, 5, 23, 16, 45, 63],[20,106,50,27,38,15]]
-print(decreaseElements(nums))
+    for i in range(len(vals)):
+        for j in range(len(vals[i])):
+            vals[i][j] -= 1
+
+    for i in range(len(vals)):
+        for j in range(len(vals[i])):
+            print(vals[i][j], end=" ")
+        print()
+
+        # testing
+DecreaseElements()
